c798252bb0a52ddc75b07ddb6796f099ebfe8481
Ashley Collis <ashleycollisburgess@gmail.com>
2020-02-12 19:06:31 -0500

    tests

--- spec/operations.test.js
+++ spec/operations.test.js
@@ -1,15 +1,19 @@
 const { add, subtract, multiply, divide } = require('../lib/operations');
 
-test('The add function should return 4 with 2 and 2', () => {
-  expect(add(2, 2)).toBe(4);
+test('The add function should return 16 with 10 and 6', () => {
+  expect(add(10, 6)).toBe(16);
 });
 
-test('The subtract function should return 1 with 2 and 1', () => {
-  expect(subtract(2, 1)).toBe(1);
+test('The subtract function should return 30 with 70 and 40', () => {
+  expect(subtract(70, 40)).toBe(30);
 });
 
-test('The divide function should return 2 with 2 and 1', () => {
-  expect(divide(2, 1)).toBe(2);
+test('The multiply function should return 120 with 12 and 10', () => {
+  expect(multiply(12, 10)).toBe(120);
+});
+
+test('The divide function should return 2 with 32 and 16', () => {
+  expect(divide(32, 2)).toBe(16);
 });
 
 test('big numbers should return scientific notation', () => {
